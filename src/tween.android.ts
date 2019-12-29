c59279cdc7f7d7210a0bc542222ebb3b1b9e5a36
Martin Guillon <martin.guillon@akylas.fr>
2019-12-29 15:00:49 +0100

    chore: refactoring

--- src/tween.android.ts
+++ src/tween.android.ts
@@ -1,102 +1,117 @@
 import TweenBase, { add, CLog, CLogTypes, Easing, getAll, Group, Interpolation, remove, removeAll, setDebug, update } from './tween.common';
 
+import { profile } from 'tns-core-modules/profiling';
+
 export { CLog, setDebug };
 
-export const TWEEN = {
-    Tween: class Tween extends TweenBase {
-        _animator: android.animation.ValueAnimator;
-        _animatorListener: android.animation.Animator.AnimatorListener;
-        _animatorUpdateListener: android.animation.ValueAnimator.AnimatorUpdateListener;
-        constructor(object) {
-            super(object);
-            this._animatorListener = new android.animation.Animator.AnimatorListener({
-                onAnimationStart: (animator: android.animation.Animator) => {
-                    CLog(CLogTypes.info, 'onAnimationStart');
-                    if (this._onStartCallback !== null) {
-                        this._onStartCallback(this._object);
-                    }
-                },
-                onAnimationRepeat: (animator: android.animation.Animator) => {
-                    CLog(CLogTypes.info, 'onAnimationRepeat');
-                },
-                onAnimationEnd: (animator: android.animation.Animator) => {
-                    CLog(CLogTypes.info, 'onAnimationEnd');
-                    this._onAnimationEnd();
-                },
-                onAnimationCancel: (animator: android.animation.Animator) => {
-                    CLog(CLogTypes.info, 'onAnimationCancel');
-                    this._onAnimationCancel();
-                }
-            });
-            // this._animator.addListener(this._animatorListener);
-            this._animatorUpdateListener = new android.animation.ValueAnimator.AnimatorUpdateListener({
-                onAnimationUpdate: (animator: android.animation.ValueAnimator) => {
-                    CLog(CLogTypes.info, 'onAnimationUpdate', animator.getAnimatedValue());
-                    this._onAnimationUpdate(animator.getAnimatedValue());
-                }
-            });
-        }
-        startAnimator() {
-            this.getOrCreateAnimator().start();
-        }
-        stopAnimator() {
-            if (this._animator) {
-                this._animator.end();
+class Tween extends TweenBase {
+    _animator: android.animation.ValueAnimator;
+    _animatorListener: android.animation.Animator.AnimatorListener;
+    _animatorUpdateListener: android.animation.ValueAnimator.AnimatorUpdateListener;
+    constructor(object) {
+        super(object);
+        this._animatorListener = new android.animation.Animator.AnimatorListener({
+            onAnimationStart: (animator: android.animation.Animator) => {
+                CLog(CLogTypes.info, 'onAnimationStart');
+                this._onAnimationStart();
+            },
+            onAnimationRepeat: (animator: android.animation.Animator) => {
+                CLog(CLogTypes.info, 'onAnimationRepeat');
+            },
+            onAnimationEnd: (animator: android.animation.Animator) => {
+                CLog(CLogTypes.info, 'onAnimationEnd');
+                this._onAnimationEnd();
+            },
+            onAnimationCancel: (animator: android.animation.Animator) => {
+                CLog(CLogTypes.info, 'onAnimationCancel');
+                this._onAnimationCancel();
             }
-        }
-        endAnimator() {
-            if (this._animator) {
-                this._animator.end();
+        });
+        // this._animator.addListener(this._animatorListener);
+        this._animatorUpdateListener = new android.animation.ValueAnimator.AnimatorUpdateListener({
+            onAnimationUpdate: (animator: android.animation.ValueAnimator) => {
+                CLog(CLogTypes.info, 'onAnimationUpdate', animator.getAnimatedValue());
+                this._onAnimationUpdate(animator.getAnimatedValue());
             }
+        });
+    }
+    startAnimator() {
+        this.getOrCreateAnimator().start();
+    }
+    stopAnimator() {
+        if (this._animator) {
+            this._animator.end();
         }
-        cancelAnimator() {
-            if (this._animator) {
-                this._animator.cancel();
-            }
+    }
+    endAnimator() {
+        if (this._animator) {
+            this._animator.end();
         }
-        pauseAnimator() {
-            if (this._animator) {
-                this._animator.pause();
-            }
+    }
+    cancelAnimator() {
+        if (this._animator) {
+            this._animator.cancel();
         }
-        resumeAnimator() {
-            if (this._animator) {
-                this._animator.resume();
-            }
+    }
+    pauseAnimator() {
+        if (this._animator) {
+            this._animator.pause();
         }
+    }
+    resumeAnimator() {
+        if (this._animator) {
+            this._animator.resume();
+        }
+    }
 
-        isPlaying() {
-            return this._animator && this._animator.isRunning();
+    isPlaying() {
+        return this._animator && this._animator.isRunning();
+    }
+    isPaused() {
+        return this._animator && !this._animator.isPaused();
+    }
+    getOrCreateAnimator() {
+        if (!this._animator) {
+            const nativeArray = Array.create('float', 2);
+            nativeArray[0] = this._reversed ? 1 : 0;
+            nativeArray[1] = this._reversed ? 0 : 1;
+            this._animator = android.animation.ValueAnimator.ofFloat(nativeArray);
+            this._animator.setInterpolator(new android.view.animation.LinearInterpolator());
+            this._animator.setDuration(this._duration);
+            this._animator.addListener(this._animatorListener);
+            this._animator.addUpdateListener(this._animatorUpdateListener);
+            // this._animator.setTarget(this);
         }
-        isPaused() {
-            return this._animator && !this._animator.isPaused();
+        return this._animator;
+    }
+    _onAnimationStart() {
+        if (this._onStartCallback !== null) {
+            this._onStartCallback(this._object);
         }
-        getOrCreateAnimator() {
-            if (!this._animator) {
-                const nativeArray = Array.create('float', 2);
-                nativeArray[0] = this._reversed ? 1 : 0;
-                nativeArray[1] = this._reversed ? 0 : 1;
-                this._animator = android.animation.ValueAnimator.ofFloat(nativeArray);
-                this._animator.setInterpolator(new android.view.animation.LinearInterpolator());
-                this._animator.setDuration(this._duration);
-                this._animator.addListener(this._animatorListener);
-                this._animator.addUpdateListener(this._animatorUpdateListener);
-                // this._animator.setTarget(this);
-            }
-            return this._animator;
+    }
+    _onAnimationEnd() {
+        if (this._onCompleteCallback !== null) {
+            this._onCompleteCallback(this._object);
         }
-        _onAnimationEnd() {
-            this._animator.removeAllListeners();
-            this._animator.removeAllUpdateListeners();
-            this._animator = null;
-            this._animator = null;
-            super._onAnimationEnd();
+        this._animator.removeAllListeners();
+        this._animator.removeAllUpdateListeners();
+        this._animator = null;
+        this._animator = null;
+        super._onAnimationEnd();
+    }
+    _onAnimationCancel() {
+        if (this._onStopCallback !== null) {
+            this._onStopCallback(this._object);
         }
-        _onAnimationCancel() {}
-        _onAnimationUpdate(delta) {
-            this.update(delta);
-        }
-    },
+    }
+
+    @profile
+    _onAnimationUpdate(delta) {
+        this.update(delta);
+    }
+}
+export const TWEEN = {
+    Tween,
     Group,
     Easing,
     Interpolation,
--- src/tween.ios.ts
+++ src/tween.ios.ts
@@ -1,104 +1,118 @@
 import TweenBase, { add, CLog, CLogTypes, Easing, getAll, Group, Interpolation, remove, removeAll, setDebug, update } from './tween.common';
+import { profile } from 'tns-core-modules/profiling';
 
 export { CLog, setDebug };
 
-export const TWEEN = {
-    Tween: class Tween extends TweenBase {
-        _animator: PMTweenUnit;
-        _paused = false;
-        _playing = false;
-        startAnimator() {
-            this.getOrCreateAnimator().startTween();
+class Tween extends TweenBase {
+    _animator: PMTweenUnit;
+    _paused = false;
+    _playing = false;
+    startAnimator() {
+        this.getOrCreateAnimator().startTween();
+    }
+    stopAnimator() {
+        if (this._animator) {
+            this._animator.stopTween();
         }
-        stopAnimator() {
-            if (this._animator) {
-                this._animator.stopTween();
-            }
+    }
+    endAnimator() {
+        if (this._animator) {
+            this._animator.stopTween();
         }
-        endAnimator() {
-            if (this._animator) {
-                this._animator.stopTween();
-            }
+    }
+    cancelAnimator() {
+        if (this._animator) {
+            this._animator.stopTween();
         }
-        cancelAnimator() {
-            if (this._animator) {
-                this._animator.stopTween();
-            }
+    }
+    pauseAnimator() {
+        if (this._animator) {
+            this._animator.pauseTween();
         }
-        pauseAnimator() {
-            if (this._animator) {
-                this._animator.pauseTween();
-            }
+    }
+    resumeAnimator() {
+        if (this._animator) {
+            this._animator.resumeTween();
         }
-        resumeAnimator() {
-            if (this._animator) {
-                this._animator.resumeTween();
-            }
-        }
+    }
 
-        isPlaying() {
-            return this._animator && this._playing;
+    isPlaying() {
+        return this._animator && this._playing;
+    }
+    isPaused() {
+        return this._animator && this._paused;
+    }
+    getOrCreateAnimator() {
+        if (!this._animator) {
+            const options = 0;
+            CLog(CLogTypes.info, 'create Tween', this._duration / 1000);
+            let startTime;
+            this._animator = PMTweenUnit.alloc().initWithPropertyStartingValueEndingValueDurationOptionsEasingBlock(null, 0, 1, this._duration / 1000, options, null);
+            this._animator.completeBlock = () => {
+                CLog(CLogTypes.info, 'completeBlock', Date.now() - startTime);
+                this._onAnimationEnd();
+            };
+            this._animator.updateBlock = () => {
+                CLog(CLogTypes.info, 'updateBlock', this._animator.tweenProgress, Date.now() - startTime);
+                this._onAnimationUpdate(this._animator.tweenProgress);
+            };
+            this._animator.startBlock = () => {
+                startTime = Date.now();
+                CLog(CLogTypes.info, 'startBlock');
+                this._onAnimationStart();
+            };
+            this._animator.stopBlock = () => {
+                CLog(CLogTypes.info, 'stopBlock', Date.now() - startTime);
+                this._onAnimationStop();
+            };
+            this._animator.pauseBlock = () => {
+                CLog(CLogTypes.info, 'pauseBlock', Date.now() - startTime);
+                this._playing = false;
+                this._paused = true;
+            };
+            this._animator.resumeBlock = () => {
+                CLog(CLogTypes.info, 'resumeBlock', Date.now() - startTime);
+                this._playing = true;
+                this._paused = false;
+            };
+            // this._animator.setTarget(this);
         }
-        isPaused() {
-            return this._animator && this._paused;
+        return this._animator;
+    }
+    _onAnimationStart() {
+        this._playing = true;
+        this._paused = false;
+        if (this._onStartCallback !== null) {
+            this._onStartCallback(this._object);
         }
-        getOrCreateAnimator() {
-            if (!this._animator) {
-                const options = 0;
-                CLog(CLogTypes.info, 'create Tween', this._duration / 1000);
-                let startTime
-                this._animator = PMTweenUnit.alloc().initWithPropertyStartingValueEndingValueDurationOptionsEasingBlock(null, 0, 1, this._duration / 1000, options, null);
-                this._animator.completeBlock = () => {
-                    CLog(CLogTypes.info, 'completeBlock', Date.now() - startTime);
-                    this._onAnimationEnd();
-                    if (this._onCompleteCallback !== null) {
-                        this._onCompleteCallback(this._object);
-                    }
-                };
-                this._animator.updateBlock = () => {
-                    CLog(CLogTypes.info, 'updateBlock', this._animator.tweenProgress, Date.now() - startTime);
-                    this.update(this._animator.tweenProgress);
-                };
-                this._animator.startBlock = () => {
-                    startTime = Date.now();
-                    CLog(CLogTypes.info, 'startBlock');
-                    this._playing = true;
-                    this._paused = false;
-                    if (this._onStartCallback !== null) {
-                        this._onStartCallback(this._object);
-                    }
-                };
-                this._animator.stopBlock = () => {
-                    CLog(CLogTypes.info, 'stopBlock', Date.now() - startTime);
-                    this._playing = false;
-                    this._paused = false;
-                    if (this._onStopCallback !== null) {
-                        this._onStopCallback(this._object);
-                    }
-                };
-                this._animator.pauseBlock = () => {
-                    CLog(CLogTypes.info, 'pauseBlock', Date.now() - startTime);
-                    this._playing = false;
-                    this._paused = true;
-                };
-                this._animator.resumeBlock = () => {
-                    CLog(CLogTypes.info, 'resumeBlock', Date.now() - startTime);
-                    this._playing = true;
-                    this._paused = false;
-                };
-                // this._animator.setTarget(this);
-            }
-            return this._animator;
+    }
+    _onAnimationEnd() {
+        if (this._onCompleteCallback !== null) {
+            this._onCompleteCallback(this._object);
         }
-        _onAnimationEnd() {
-            this._animator = null;
-            super._onAnimationEnd();
+        this._animator = null;
+        super._onAnimationEnd();
+    }
+    // _onAnimationCancel() {
+    //     if (this._onStopCallback !== null) {
+    //         this._onStopCallback(this._object);
+    //     }
+    // }
+    _onAnimationStop() {
+        this._playing = false;
+        this._paused = false;
+        if (this._onStopCallback !== null) {
+            this._onStopCallback(this._object);
         }
-        _onAnimationCancel() {}
-        _onAnimationUpdate(delta) {
-            this.update(delta);
-        }
-    },
+    }
+    @profile
+    _onAnimationUpdate(delta) {
+        this.update(delta);
+    }
+}
+
+export const TWEEN = {
+    Tween: Tween,
     Group,
     Easing,
     Interpolation,
