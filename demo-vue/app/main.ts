31f005a040c00881f4cde86573ff3bc6cafd047b
Martin Guillon <martin.guillon@akylas.fr>
2019-12-29 15:01:03 +0100

    test: demo app

--- demo-vue/app/main.ts
+++ demo-vue/app/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'nativescript-vue';
 import * as views from './views';
 
+import * as trace from 'tns-core-modules/trace';
+trace.addCategories(trace.categories.NativeLifecycle);
+trace.addCategories(trace.categories.Layout);
+// trace.enable();
 
 Vue.component(views.Home.name, views.Home);
 new Vue({
